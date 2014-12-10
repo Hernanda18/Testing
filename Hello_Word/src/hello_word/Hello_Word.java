ad41ca62a835356dfbf8b15ee18146ef29de9ca6
hernanda <Novriana@hotmail.com>
2014-12-10 15:43:20 +0700

    sudah bisa looping

---
+++ Hello_Word/src/hello_word/Hello_Word.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_word;
+
+/**
+ *
+ * @author Hernanda-snoop
+ */
+public class Hello_Word {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i = 1 ; i<6;i++)
+        System.out.println("testing"+i);
+    }
+}
