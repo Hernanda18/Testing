600ca9d930846c0dca35e95b78a040259996cfd0
hernanda <Novriana@hotmail.com>
2014-12-18 19:44:19 +0700

    Berubah

---
+++ Testing_github/src/testing_github/Testing_github.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing_github;
+
+/**
+ *
+ * @author Hernanda-snoop
+ */
+public class Testing_github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=0 ; i<5 ;i++)
+            System.out.println("Testing Githubs");
+    }
+}
